4dc02544094d9fc56f30ff51e018a64a9b8bb36b
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-18 13:33:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList||webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent||webkitSpeechRecognitionEvent
 var recognition = new SpeechRecognition();
+recognition.continuous =true;
+recognition.interimResults = true;
+recognition.lang="en-US";
+const notes =document.getElementsByClassName('notes');
+
 
 //when recog starts running
 
 recognition.onstart=function(){
-
+    console.log("Speech Recognition is now running")
 }
 
 //when user finishes talking;
 recognition.onspeechend=function(){
-
-    recognition.stop();
+    console.log("Speech Recognition has ended")
 }
-
+recognition.onerror=function(e){
+    console.error(e)
+}
 //when results are returned;
+recognition.onresult=function(e){
+    var transcript = e.results[0][0].transcript;
+    notes.textContent +=transcript;
+    notes.value =notes.textContent
+}
+$(".start").on('click',function(e){
+    recognition.start()
+})
+$(".pause").on('click',function(e){
+    recognition.stop();
+})
